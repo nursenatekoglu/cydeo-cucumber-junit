a306b0372cb94af2af72cfa122a3dea89721510d
nursena <nursena.tekoglu@gmail.com>
2023-02-19 15:22:14 +0000

    second commit

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -1,13 +1,16 @@
 package com.cydeo.runners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.*;
+import org.junit.runner.*;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions"
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = "@employee"
+
 )
 public class CukesRunner {
 }
