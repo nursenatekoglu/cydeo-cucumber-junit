f287706a865bdd69683f2c1ff138d9fa0df927fa
nursena <nursena.tekoglu@gmail.com>
2023-02-20 23:43:01 +0000

    day 16 is done

---
+++ src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -0,0 +1,23 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class WikiSearchPage {
+
+    public WikiSearchPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "searchInput")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement searchButton;
+
+    @FindBy(id = "firstHeading")
+    public WebElement mainHeader;
+
+
+}
